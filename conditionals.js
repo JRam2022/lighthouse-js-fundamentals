f356081f3d238fd406bf7b12fcf4ab3a7f8cd570
JRam2022 <jasonram778@gmail.com>
2022-03-03 00:24:13 -0800

    Fixed

--- conditionals.js
+++ conditionals.js
@@ -24,3 +24,15 @@ if (temperature < -40 || temperature > 40) {
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+const whichSchool  = function(age) {
+  if (age >= 1 && age <= 12) {
+    return "Elementary School";
+  } else if (age => 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age => 19 && age <= 100) {
+    return "Lighthouse Labs";
+  }
+} 
+  
+console.log(whichSchool);
