1fa8bdf28d44dcf145722a9ce2001d8d4b464049
JRam2022 <jasonram778@gmail.com>
2022-03-05 20:37:05 -0800

    Loopy Lighthouse exercise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,17 @@
+ let num;
+
+ for (num = 100; num <= 200; num++) {
+   if (num % 4 === 0 && num % 3 === 0) {
+     console.log("LoopyLighthouse");
+   } else if (num % 4 === 0) {
+     console.log("Lighthouse");
+   } else if (num % 3 === 0) {
+     console.log("Loopy");
+   } else{
+     console.log(num)
+   }
+   
+   }
+   
+  
+ 
